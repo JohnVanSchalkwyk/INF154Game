97fb6534267906f0675b830cb278ec8ece81c330
shiznet101.darren@gmail.com <Admin1994>
2015-03-30 21:45:08 +0200

    Sets Race

---
+++ INF154Game/setRace.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF154Game
+{
+    public class setRace
+    {
+       public void raceMain()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
