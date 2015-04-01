0937a8d932dd062293e44f09c6d249f6d211f9d8
shiznet101.darren@gmail.com <Admin1994>
2015-04-01 15:25:44 +0200

    Comments added

--- INF154Game/setCursor.cs
+++ INF154Game/setCursor.cs
@@ -8,14 +8,14 @@ namespace INF154Game
 {
     class setCursor
     {
-        protected  int origRow;     //Co-ordinates of the screen
-        protected  int origCol;
+        protected  int origRow = 0;     //Co-ordinates of the screen
+        protected  int origCol = 0;
 
         public void WriteAt(string s, int x, int y)
         {
             try
             {
-                Console.SetCursorPosition(origCol + x, origRow + y); // Set the cursor position IE - (column,row) -- We can jump back to column 1 
+                Console.SetCursorPosition(origCol + x, origRow + y); // Set the cursor position IE - (x,y) -- We can jump back to column 1 
                                                                      // while staying at the same row position and vice versa.
                 Console.Write(s);
             }
